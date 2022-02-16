ca9fc09831911a2d2a2206a8fd42155f4a21af9f
Jimmy Rando <jsturat46925@gmail.com>
2022-02-16 18:49:31 -0500

    added table left join

--- server.js
+++ server.js
@@ -32,7 +32,11 @@ const db= mysql.createConnection(
 //GET all candidates (express.js route)
 app.get('/api/candidates', (req, res) => 
 {
-    const sql= `SELECT * FROM candidates`;
+    const sql= `SELECT candidates.*, parties.name 
+                AS party_name
+                FROM candidates 
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id`;
 
     db.query(sql, (err, rows) => 
     {
@@ -62,7 +66,12 @@ app.get('/api/candidates', (req, res) =>
 // GET a single candidate (express.js route)
 app.get('/api/candidates/:id', (req, res)=>
 {
-    const sql= `SELECT * FROM candidates WHERE id = ?`;
+    const sql= `SELECT candidates.*, parties.name 
+                AS party_name
+                FROM candidates 
+                LEFT JOIN parties
+                ON candidates.party_id = parties.id
+                WHERE id = ?`;
     const params= [req.params.id];
 
     db.query(sql, params, (err, row)=>
